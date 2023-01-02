21642cd22c9a13bad2df3a4218633b9d89c32210
Denys Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2023-01-02 17:59:39 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,12 +10,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				
-				destination:'https://cinema-api-production.up.railway.app/:path*',
+				destination: 'https://cinema-api-production.up.railway.app/:path*',
 			},
 			{
 				source: '/uploads/:path*',
-				destination: `https://cinema-api-production.up.railway.app/uploads/:path*`,
+				destination: 'https://cinema-api-production.up.railway.app/uploads/:path*',
 				
 			},
 		]
